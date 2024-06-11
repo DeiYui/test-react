22fef92d1456174fe0afa0e1a313f16f7d1ab27e
DeiYui <www.doducduy@gmail.com>
2024-06-12 00:21:33 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.scss';
 import Header from './component/Header/Header';
-const App = () => {
+import { Link } from 'react-router-dom';
 
+const App = () => {
 
   return (
     <div className="app-container">
       <Header />
+      <div>
+        Link
+        <div>
+          <button>
+          <Link to="/users">den user</Link>
+          </button>
+          <button>
+          <Link to="/admins">den admin</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
